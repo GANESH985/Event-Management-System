8aa926d86e87fb352896fc1f5beaf991752d4b28
Ganesh <nayakg985@gmail.com>
2025-11-08 12:12:53 +0530

    Updated

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,12 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI;
-    const conn = await mongoose.connect(uri)
+    const conn = await mongoose.connect(uri,
+      {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+    )
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
